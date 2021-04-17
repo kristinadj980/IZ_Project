bb3da4cef8a7a7a18971ed04d0c524e2c8a35f5b
David Drvar <david.drvar.bogdanovic@gmail.com>
2021-04-17 15:30:05 +0200

    project initialization

---
+++ knowledge-engineering/src/main/java/com/knowledgeEngineering/knowledgeengineering/KnowledgeEngineeringApplication.java
@@ -0,0 +1,13 @@
+package com.knowledgeEngineering.knowledgeengineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeEngineeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeEngineeringApplication.class, args);
+	}
+
+}
