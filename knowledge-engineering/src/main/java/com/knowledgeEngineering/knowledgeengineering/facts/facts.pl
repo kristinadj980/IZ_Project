97aa041e5855a687dbb97839a808ce1043367318
David Drvar <david.drvar.bogdanovic@gmail.com>
2021-04-19 21:35:18 +0200

    pl file v1.1

--- knowledge-engineering/src/main/java/com/knowledgeEngineering/knowledgeengineering/facts/facts.pl
+++ knowledge-engineering/src/main/java/com/knowledgeEngineering/knowledgeengineering/facts/facts.pl
@@ -60,12 +60,116 @@ attack(pretexting-via-customer-service, company-sector-technology,23).
 attack(pretexting-via-customer-service, company-sector-business-and-professional-service,53).
 attack(pretexting-via-customer-service, company-sector-retail-and-manifacturing,76).
 attack(pretexting-via-customer-service, company-sector-logistics-and-transportation,23).
-attack(cross-site-flashing, europe, 35).
-attack(cross-site-flashing, north-america, 87).
-attack(cross-site-flashing, south-america, 12).
-attack(cross-site-flashing, australia, 93).
-attack(cross-site-flashing, asia, 75).
-attack(cross-site-flashing, africa, 7).
+attack(pretexting-via-customer-service, europe, 35).
+attack(pretexting-via-customer-service, north-america, 87).
+attack(pretexting-via-customer-service, south-america, 12).
+attack(pretexting-via-customer-service, australia, 93).
+attack(pretexting-via-customer-service, asia, 75).
+attack(pretexting-via-customer-service, africa, 7).
+
+
+%dns-domain-seizure
+attack(dns-domain-seizure, update, 63).
+attack(dns-domain-seizure, suspicious-conversation-phone-call, 7).
+attack(dns-domain-seizure, suspicious-conversation-phone-message, 8).
+attack(dns-domain-seizure, suspicious-conversation-email, 6).
+attack(dns-domain-seizure, suspicious-conversation-visit, 2).
+attack(dns-domain-seizure, app-download, 6).
+attack(dns-domain-seizure, ad-click, 0).
+attack(dns-domain-seizure, ad-blocker-deactivation, 0).
+attack(dns-domain-seizure, suspicious-link, 23).
+attack(dns-domain-seizure, pop-up-windows, 21).
+attack(dns-domain-seizure, credential-re-entering, 79).
+attack(dns-domain-seizure, services-fail, 99).
+attack(dns-domain-seizure, credentials-theft, 83).
+attack(dns-domain-seizure, suspicious-code-modifications, 53).
+attack(dns-domain-seizure, frequents-spams, 11).
+attack(dns-domain-seizure, company-size-1-250, 11).
+attack(dns-domain-seizure, company-size-250-500, 7).
+attack(dns-domain-seizure, company-size-500-1000, 37).
+attack(dns-domain-seizure, company-size-1000+,75).
+attack(dns-domain-seizure, company-sector-hospital, 52).
+attack(dns-domain-seizure, company-sector-finance,88).
+attack(dns-domain-seizure, company-sector-technology, 23).
+attack(dns-domain-seizure, company-sector-business-and-professional-service,73).
+attack(dns-domain-seizure, company-sector-retail-and-manifacturing,48).
+attack(dns-domain-seizure, company-sector-logistics-and-transportation, 89).
+attack(dns-domain-seizure, europe, 53).
+attack(dns-domain-seizure, north-america, 87).
+attack(dns-domain-seizure, south-america, 12).
+attack(dns-domain-seizure, australia, 76).
+attack(dns-domain-seizure, asia, 75).
+attack(dns-domain-seizure, africa, 17).
+
+
+%influence-via-incentives
+attack(influence-via-incentives, update, 13).
+attack(influence-via-incentives, suspicious-conversation-phone-call, 97).
+attack(influence-via-incentives, suspicious-conversation-phone-message, 98).
+attack(influence-via-incentives, suspicious-conversation-email, 91).
+attack(influence-via-incentives, suspicious-conversation-visit, 85).
+attack(influence-via-incentives, app-download, 6).
+attack(influence-via-incentives, ad-click, 0).
+attack(influence-via-incentives, ad-blocker-deactivation, 0).
+attack(influence-via-incentives, suspicious-link, 89).
+attack(influence-via-incentives, pop-up-windows, 13).
+attack(influence-via-incentives, credential-re-entering, 56).
+attack(influence-via-incentives, services-fail, 44).
+attack(influence-via-incentives, credentials-theft, 83).
+attack(influence-via-incentives, suspicious-code-modifications, 13).
+attack(influence-via-incentives, frequents-spams, 11).
+attack(influence-via-incentives, company-size-1-250, 65).
+attack(influence-via-incentives, company-size-250-500, 17).
+attack(influence-via-incentives, company-size-500-1000,37).
+attack(influence-via-incentives, company-size-1000+,5).
+attack(influence-via-incentives, company-sector-hospital, 2).
+attack(influence-via-incentives, company-sector-finance,8).
+attack(influence-via-incentives, company-sector-technology, 43).
+attack(influence-via-incentives, company-sector-business-and-professional-service,83).
+attack(influence-via-incentives, company-sector-retail-and-manifacturing,78).
+attack(influence-via-incentives, company-sector-logistics-and-transportation, 39).
+attack(influence-via-incentives, europe, 53).
+attack(influence-via-incentives, north-america, 87).
+attack(influence-via-incentives, south-america, 12).
+attack(influence-via-incentives, australia, 76).
+attack(influence-via-incentives, asia, 75).
+attack(influence-via-incentives, africa, 17).
+
+
+%harvesting-information-via-api-event-monitoring
+attack(harvesting-information-via-api-event-monitoring, update, 3).
+attack(harvesting-information-via-api-event-monitoring, suspicious-conversation-phone-call, 98).
+attack(harvesting-information-via-api-event-monitoring, suspicious-conversation-phone-message, 84).
+attack(harvesting-information-via-api-event-monitoring, suspicious-conversation-email, 63).
+attack(harvesting-information-via-api-event-monitoring, suspicious-conversation-visit, 79).
+attack(harvesting-information-via-api-event-monitoring, app-download, 70).
+attack(harvesting-information-via-api-event-monitoring, ad-click, 30).
+attack(harvesting-information-via-api-event-monitoring, ad-blocker-deactivation, 0).
+attack(harvesting-information-via-api-event-monitoring, suspicious-link, 41).
+attack(harvesting-information-via-api-event-monitoring, pop-up-windows, 2).
+attack(harvesting-information-via-api-event-monitoring, credential-re-entering, 79).
+attack(harvesting-information-via-api-event-monitoring, services-fail, 31).
+attack(harvesting-information-via-api-event-monitoring, credentials-theft, 81).
+attack(harvesting-information-via-api-event-monitoring, suspicious-code-modifications, 5).
+attack(harvesting-information-via-api-event-monitoring, frequents-spams, 71).
+attack(harvesting-information-via-api-event-monitoring, company-size-1-250, 56).
+attack(harvesting-information-via-api-event-monitoring, company-size-250-500, 73).
+attack(harvesting-information-via-api-event-monitoring, company-size-500-1000,13).
+attack(harvesting-information-via-api-event-monitoring, company-size-1000+,75).
+attack(harvesting-information-via-api-event-monitoring, company-sector-hospital, 32).
+attack(harvesting-information-via-api-event-monitoring, company-sector-finance,58).
+attack(harvesting-information-via-api-event-monitoring, company-sector-technology, 73).
+attack(harvesting-information-via-api-event-monitoring, company-sector-business-and-professional-service,71).
+attack(harvesting-information-via-api-event-monitoring, company-sector-retail-and-manifacturing,35).
+attack(harvesting-information-via-api-event-monitoring, company-sector-logistics-and-transportation, 9).
+attack(harvesting-information-via-api-event-monitoring, europe, 77).
+attack(harvesting-information-via-api-event-monitoring, north-america, 87).
+attack(harvesting-information-via-api-event-monitoring, south-america, 92).
+attack(harvesting-information-via-api-event-monitoring, australia, 72).
+attack(harvesting-information-via-api-event-monitoring, asia, 15).
+attack(harvesting-information-via-api-event-monitoring, africa, 98).
+
+
 
 %mere zastite----------------------------------------------------------------------
 
@@ -78,4 +182,14 @@ countermeasure(cross-site-flashing, cross-domain-xml-configuration,75).
 countermeasure(pretexting-via-customer-service, validate-phone-number, 95).
 countermeasure(pretexting-via-customer-service, employee-education,47).
 
+%dns-domain-seizure
+countermeasure(dns-domain-seizure, contact-emergency-services, 95).
+countermeasure(dns-domain-seizure, restore-from-back-up, 77).
+
+%influence-via-incentives
+countermeasure(influence-via-incentives, educate-employees, 81).
+countermeasure(influence-via-incentives, give-only-required-information, 59).
+
+%harvesting-information-via-api-event-monitoring
+countermeasure(harvesting-information-via-api-event-monitoring, encrypt-data-transmission,93).
 
