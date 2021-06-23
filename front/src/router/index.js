4374a0a08bf511259829c02c0a4634cb194f4cd2
Kristina Djuric <kristinadjuriic@gmail.com>
2021-06-23 14:23:43 +0200

    front vue2

---
+++ front/src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Cbr from '../views/Cbr.vue'
+import Bayesian from '../views/Bayesian.vue'
+import Fuzzy from '../views/Fuzzy.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
+  },
+  {
+    path: '/cbr',
+    name: 'Cbr',
+    component: Cbr
+  },
+  {
+    path: '/bayesian',
+    name: 'Bayesian',
+    component: Bayesian
+  },
+  {
+    path: '/fuzzy',
+    name: 'Fuzzy',
+    component: Fuzzy
+  },
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+  })
+  
+  export default router
